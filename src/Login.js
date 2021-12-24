278ecc5165c1170f0a5e37ffa0dcc897434ccc9b
arif034 <45455917+arif034@users.noreply.github.com>
2021-12-24 23:37:42 +0530

    working code working code

--- src/Login.js
+++ src/Login.js
@@ -1,57 +1,22 @@
-import { Component, useReducer } from "react";
+import { Component } from "react";
 import Navbar from "./Navbar";
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "",
-      lastName: "",
-      gender: "",
-      password: "",
-      confirm_password: ""
+      firstName: ""
     };
   }
 
-  onChangeConfirmPassword = (event) => {
+  changeFirstNameHandler = (event) => {
     this.setState({
-      confirm_password: event.target.value.confirm_password
+      firstName: event.target.value
     });
   };
 
-  onChangePassword = (event) => {
-    this.setState({
-      password: event.target.value.password
-    });
-  };
-
-  onChangeConfirmGender = (event) => {
-    this.setState({
-      gender: event.target.value.gender
-    });
-  };
-
-  onChangeFirstName = (event) => {
-    this.setState({
-      firstName: event.target.value.firstName
-    });
-  };
-
-  onChangeLastName = (event) => {
-    this.setState({
-      firstName: event.target.value.lastName
-    });
-  };
-
-  onChangeConfirmPassword = (event) => {
-    this.setState({
-      confirm_password: event.target.value.confirm_password
-    });
-  };
-
-  onSubmit = () => {
-    if (localStorage.getItem("user"))
-      localStorage.setItem("user", [localStorage.getItem("user"), this.state]);
-    else localStorage.setItem("user", this.state);
+  onChange = (event) => {
+    alert(this.state.firstName);
+    event.preventDefault();
   };
 
   render() {
@@ -59,53 +24,16 @@ class Login extends Component {
       <div>
         <Navbar />
         <form>
-          <label>First Name</label>
+          <label>FirstName</label>
+          <br />
           <br />
           <input
-            type="text"
-            placeholder="First Name"
             value={this.state.firstName}
-            onChange={this.onChangeFirstName}
-          />
-          <br />
-          <label>Last Name</label>
-          <br />
-          <input
-            type="text"
-            placeholder="Last Name"
-            value={this.state.lastName}
-            onChange={this.onChangeLastName}
-          />
-          <br />
-          <label>Gender</label>
-          <br />
-          <input
-            type="text"
-            placeholder="Gender"
-            value={this.state.gender}
-            onChange={this.onChangeGender}
-          />
-          <br />
-          <label>Password</label>
-          <br />
-          <input
-            type="text"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.onChangePassword}
-          />
-          <br />
-          <label>Confirm Password</label>
-          <br />
-          <input
-            type="text"
-            placeholder="Confirm Password"
-            value={this.state.confirm_password}
-            onChange={this.onChangeConfirmPassword}
+            onChange={this.changeFirstNameHandler}
           />
           <br />
           <br />
-          <button onClick={this.onSubmit}>Submit</button>
+          <button onClick={this.onChange}>Submit</button>
         </form>
       </div>
     );
